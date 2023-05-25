e635b0d07239eaf550401c2281cd4b90ddd575ad
ayseleynavuz <ayseleynavuz@gmail.com>
2023-05-25 20:21:37 +0300

    Template Engine Added

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
-app.get('/', (req, res) => {
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs'); // set the view engine to ejs
 
-  const blog = {
-    id: 1,
-    name: "Blog Title",
-    description: "Blog description"
-  }
-  res.send(blog)
-})
+// MIDDLEWARE
+app.use(express.static('public')); // to access the files in public folder
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+app.get('/post', (req, res) => {
+    res.render('post');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
 
 const port = 3000;
+
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı..`);
+  console.log(`Server is running on port ${port}`);
 });
