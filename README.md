f11e6d9f8520d2b626545ba061d3a637e8ca576d
ayseleynavuz <ayseleynavuz@gmail.com>
2023-05-31 16:48:56 +0300

    mongoose connection

---
+++ README.md
@@ -0,0 +1,17 @@
+# cleanBlog Project
+
+## Used Technologies and Techniques
+- Prettier
+- Github
+- Nodemon
+- Statik files
+- Template Engine
+- Middlewares
+- Express
+- MongoDB
+- Mongoose
+- CRUD
+- MVC
+- Pagination 
+- Atlas MongoDB cloud
+- Heroku Deployment
--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const ejs = require('ejs');
-
+const mongoose = require('mongoose');
 const app = express();
+const Post = require("./models/Post");
+
+//connect DB
+mongoose.connect("mongodb://127.0.0.1:27017/cleanblog-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(db => console.log('DB is connected'))
+.catch(err => console.log(err));
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs'); // set the view engine to ejs
 
 // MIDDLEWARE
 app.use(express.static('public')); // to access the files in public folder
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 //ROUTES
 app.get('/', (req, res) => {
@@ -23,7 +35,13 @@ app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
 
-const port = 3000;
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
+});
+
+
+const port = 3001;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
